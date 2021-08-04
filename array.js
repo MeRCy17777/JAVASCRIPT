e3efc2a06f062c542ca8f251af1e008941871636
MeRCy17777 <86611689+MeRCy17777@users.noreply.github.com>
2021-08-04 12:19:30 +0530

    Day2 Assignment

---
+++ array.js
@@ -0,0 +1,11 @@
+let array=["Ben","Jen","Zen","Ken","Yen"]
+console.log("Given array")
+for(let i=0;i<array.length;i++)
+{
+    console.log(`${array[i]}`)
+}
+console.log("Sorted array")
+for(let i=array.length-1;i>=0;i--)
+{
+    console.log(array[i])
+}
