61ff42f7751dc42c00b1ff65b246f9eae44fc343
MeRCy17777 <86611689+MeRCy17777@users.noreply.github.com>
2021-08-05 10:00:23 +0530

    Day3 Assignment

---
+++ dom.js
@@ -0,0 +1,9 @@
+// console.log(document.querySelectorAll('h1'));
+
+function eve()
+{
+  let nam= document.querySelector("#one").value;
+  let cit= document.querySelector("#two").value;
+document.querySelector("#squ").innerHTML=`<h3>NAME  :${nam}   CITY :${cit}</h3> `;
+; 
+}
